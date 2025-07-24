6309d5cddc253fda2f8f45fdd4d630bd97054e77
vitor gamarano <vitorgamarano1@gmail.com>
2025-07-24 08:49:38 -0300

    Ajustando as rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { Navbar } from './components/Navbar'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import  TemperatureConverter  from './pages/Temperatura'
-import DistanceConverter from './pages/Distancia'
-import WeightConverter  from './pages/Peso'
-import  Home  from './pages/Home'
+import  Navbar  from './components/Navbar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import  TemperatureConverter  from './pages/Temperatura';
+import DistanceConverter from './pages/Distancia';
+import WeightConverter  from './pages/Peso';
+import  Home  from './pages/Home';
 import { Container } from "react-bootstrap";
 
 function App() {
@@ -20,11 +20,8 @@ function App() {
           <Route path="/Temperatura" element={<TemperatureConverter />} />
           <Route path="/Peso" element={<WeightConverter />} />
       </Routes>
-
     </Container>
-    <DistanceConverter />
-    <TemperatureConverter />
-    <WeightConverter />
+   
   
    
     </BrowserRouter>
